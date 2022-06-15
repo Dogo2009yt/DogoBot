d827816e41750278f21ca785cd0e20c697712752
Dogo2009yt <107557595+Dogo2009yt@users.noreply.github.com>
2022-06-15 14:59:59 +0200

    DogoBot

---
+++ index.js
@@ -0,0 +1,26 @@
+ const Discord = require("discord.js")
+ const client = new Discord.Client(
+    {intents:["GUILDS","GUILD_MEMBERS","GUILD_MESSAGES"]}
+ )
+
+ client.login("OTg1NjQzODc4MDgwMjA0ODUw.GUGWDs.s8KmqO6i30KU-5ZaHp4Sy5ZBixil1yi7FiCkYk")
+
+client.on("ready",() => {
+   console.log ("bot online")
+
+
+
+
+}) 
+client.on("messageCreate", (message) => {
+  if (message.content == "!youtube") (
+     message.channel.send(`${message.author.username} questo è il mio canale yt:youtube.com/channel/UC-fRd6-frGO5jlo56jVSiyg`)
+  )
+  if (message.content == "!ciao") (
+   message.channel.send("Ciao anche a te")
+  )
+  
+})
+
+
+
