2200bd41f08d62eb3878db26efec08177a630250
Dogo2009yt <107557595+Dogo2009yt@users.noreply.github.com>
2023-10-06 22:14:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ global.distube = new DisTube(client, {
 })
 
 
- client.login("OTg1NjQzODc4MDgwMjA0ODUw.GUGWDs.s8KmqO6i30KU-5ZaHp4Sy5ZBixil1yi7FiCkYk")
+ client.login("OTg1NjQzODc4MDgwMjA0ODUw.G1jqtB.ZCZszYC29eCfrK838GQeEPUUHpE2t-VLC6pjc8")
 
 client.on("ready",() => {
    console.log ("bot online")
